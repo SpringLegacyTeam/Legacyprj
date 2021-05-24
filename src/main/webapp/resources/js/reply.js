91f511227756564e87900eebc9ebe13d6c486f1c
윤주영 <pulpul8282@naver.com>
2021-05-24 13:18:33 +0900

    커밋테스트

--- src/main/webapp/resources/js/reply.js
+++ src/main/webapp/resources/js/reply.js
@@ -1,48 +1,31 @@
-console.log("Reply Module........");
+console.log("댓글 확인");
 
-var replyService = (function() {
 
-	function add(reply, callback, error) {
-		console.log("add reply...............");
-
-		$.ajax({
-			type : 'post',
-			url : '/replies/new',
-			data : JSON.stringify(reply),
-			contentType : "application/json; charset=utf-8",
-			success : function(result, status, xhr) {
-				if (callback) {
-					callback(result);
-				}
+var replyService=(function(){
+	//댓글 추가=============================================================
+	function add(reply, callback, error){
+		console.log("댓글 추가")
+		
+		$.ajax({ 
+			type: 'post',
+			url: '/replies/new',
+			data: JSON.stringify(reply), 
+			contentType: "application/json; charset=utf-8",
+			success: function(result, status, xhr){
+			 if(callback){
+			 	callback(result);
+			 }
 			},
-			error : function(xhr, status, er) {
-				if (error) {
+			error: function(xhr, status, er){
+				if(error){
 					error(er);
 				}
 			}
-		})
-	}
-
-//	function getList(param, callback, error) {
-//
-//		var bno = param.bno;
-//		var page = param.page || 1;
-//
-//		$.getJSON("/replies/pages/" + bno + "/" + page + ".json",
-//				function(data) {
-//					if (callback) {
-//						callback(data);
-//					}
-//				}).fail(function(xhr, status, err) {
-//			if (error) {
-//				error();
-//			}
-//		});
-//	}
+		});//end ajax
+	} // end function add
 	
-	
-
-	function getList(param, callback, error) {
+	//댓글 조회=============================================================
+		function getList(param, callback, error) {
 
 	    var bno = param.bno;
 	    var page = param.page || 1;
@@ -59,127 +42,88 @@ var replyService = (function() {
 	        error();
 	      }
 	    });
-	  }
-
+	  } // end getList
 	
-//	function remove(rno, callback, error) {
-//		$.ajax({
-//			type : 'delete',
-//			url : '/replies/' + rno,
-//			success : function(deleteResult, status, xhr) {
-//				if (callback) {
-//					callback(deleteResult);
-//				}
-//			},
-//			error : function(xhr, status, er) {
-//				if (error) {
-//					error(er);
-//				}
-//			}
-//		});
-//	}
-	
-	  function remove(rno, replyer, callback, error) {
-		  
-		  
-		console.log("--------------------------------------");  
-		console.log(JSON.stringify({rno:rno, replyer:replyer}));  
-		    
-	    $.ajax({
-	      type : 'delete',
-	      url : '/replies/' + rno,
-	      
-	      data:  JSON.stringify({rno:rno, replyer:replyer}),
-	      
-	      contentType: "application/json; charset=utf-8",
-	      
-	      success : function(deleteResult, status, xhr) {
-	        if (callback) {
-	          callback(deleteResult);
-	        }
-	      },
-	      error : function(xhr, status, er) {
-	        if (error) {
-	          error(er);
-	        }
-	      }
-	    });
-	  }
-
-
-	function update(reply, callback, error) {
-
-		console.log("RNO: " + reply.rno);
-
+	//댓글 삭제=============================================================
+	function remove(rno, callback, error){
 		$.ajax({
-			type : 'put',
-			url : '/replies/' + reply.rno,
-			data : JSON.stringify(reply),
-			contentType : "application/json; charset=utf-8",
-			success : function(result, status, xhr) {
-				if (callback) {
-					callback(result);
+		type : 'delete',
+		url : '/replies/' + rno,
+		success : function(deleteResult, status, xhr){
+			if(callback){
+			callback(deleteResult);
+			}
+		},
+		error : function(xhr, status, er){
+			if(error){
+				error(er);
+			}
+		}
+		});
+	} // end remove
+	
+	//댓글 수정==================================================================
+	function update(reply, callback, error){
+		console.log("댓글번호: "+reply.rno);
+		
+		$.ajax({
+			type: 'put',
+			url: '/replies/' + reply.rno,
+			data: JSON.stringify(reply),
+			contentType: "application/json; charset=utf-8",
+			success: function(result, status, xhr){
+				if(callback){
+				callback(result);
 				}
 			},
-			error : function(xhr, status, er) {
-				if (error) {
-					error(er);
-				}
+			error : function(xhr, status, er){
+			error(er);
 			}
 		});
-	}
-
-	function get(rno, callback, error) {
-
-		$.get("/replies/" + rno + ".json", function(result) {
-
-			if (callback) {
+		}
+	
+	//특정 번호댓글 조회==================================================================
+	function get(rno, callback, error){
+		
+		$.get("/replies/" + rno + ".json", function(result){
+			if(callback){
 				callback(result);
 			}
-
-		}).fail(function(xhr, status, err) {
-			if (error) {
+		}).fail(function(xhr, status, err){
+			if(error){
 				error();
 			}
 		});
 	}
-
-	function displayTime(timeValue) {
-
+	
+	//XML JSON 형태 시간처리==================================================================
+	function displayTime(timeValue){
 		var today = new Date();
-
 		var gap = today.getTime() - timeValue;
-
 		var dateObj = new Date(timeValue);
 		var str = "";
-
-		if (gap < (1000 * 60 * 60 * 24)) {
-
+		
+		if(gap < (1000 * 60 * 60 *24)){
 			var hh = dateObj.getHours();
 			var mi = dateObj.getMinutes();
 			var ss = dateObj.getSeconds();
-
-			return [ (hh > 9 ? '' : '0') + hh, ':', (mi > 9 ? '' : '0') + mi,
-					':', (ss > 9 ? '' : '0') + ss ].join('');
-
-		} else {
+			
+			return[(hh>9 ? '':'0')+hh, ':', (mi > 9 ? '':'0') + mi, ':', (ss > 9? '':'0')+ss ].join('');
+		}else{
 			var yy = dateObj.getFullYear();
-			var mm = dateObj.getMonth() + 1; // getMonth() is zero-based
+			var mm = dateObj.getMonth()+1;
 			var dd = dateObj.getDate();
-
-			return [ yy, '/', (mm > 9 ? '' : '0') + mm, '/',
-					(dd > 9 ? '' : '0') + dd ].join('');
-		}
+			
+			return [yy, '/', (mm>9 ? '':'0') + mm, '/',(dd>9 ?'':'0')+dd].join('');
+		}		
 	}
-	;
-
+	
 	return {
 		add : add,
-		get : get,
 		getList : getList,
 		remove : remove,
 		update : update,
+		get : get,
 		displayTime : displayTime
 	};
-
 })();
